eef1ff48a951d0e9761e8eeb6d99bd77fb69d763
meheraj786 <meherajhosen786@gmail.com>
2025-09-06 20:31:58 +0600

    navbar add

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import Flex from '../../layouts/Flex'
+import Container from '../../layouts/Container'
 import Logo from '../../layouts/Logo'
 import { NavLink } from 'react-router'
+import { GiLoveHowl } from 'react-icons/gi'
+import { PiCarThin } from 'react-icons/pi'
+import { BiCart, BiHeart } from 'react-icons/bi'
+
 
 const Navbar = () => {
   const navItems=[
@@ -23,9 +28,11 @@ const Navbar = () => {
     },
   ]
   return (
-    <div className='font-body'>
+    <div className='font-body mt-[47px] pb-[23px] border-b border-black/30'>
+      <Container>
       <Flex>
       <Logo/>
+      <Flex className="justify-center gap-x-[48px]">
       {
         navItems.map((item)=>(
           <NavLink to={item.link} className={({isActive})=>isActive ? "border-b":"hover:border-b"}>
@@ -33,8 +40,14 @@ const Navbar = () => {
           </NavLink>
         ))
       }
-
       </Flex>
+      <Flex className="justify-end gap-x-5">
+        <input type="text" className='bg-[#F5F5F5] px-5 py-3 pr-[70px] rounded-s' placeholder='What are you looking for?' />
+        <BiHeart/>
+        <BiCart/>
+      </Flex>
+      </Flex>
+      </Container>
     </div>
   )
 }
--- src/layouts/Button.jsx
+++ src/layouts/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({children, className}) => {
   return (
-    <div className={`px-[48px] py-[16px] bg-primary text-white font-body font-medium rounded-[4px] cursor-pointer hover:bg-orange-700 transition-colors duration-150 ${className}`}>{children || "View Products"}</div>
+    <button className={`px-[48px] py-[16px] bg-primary text-white font-body font-medium rounded-[4px] cursor-pointer hover:bg-orange-700 transition-colors duration-150 ${className}`}>{children || "View Products"}</button>
   )
 }
 
